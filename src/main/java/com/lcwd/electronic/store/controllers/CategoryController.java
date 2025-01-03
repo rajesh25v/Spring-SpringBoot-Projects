dfd887225f2adf0cebee47df795d2efe40d74349
vadlamudi rajesh <rajesh648n@gmail.com>
2025-01-03 22:57:36 +0530

    mya-7165 NFA fix BugFix12

--- src/main/java/com/lcwd/electronic/store/controllers/CategoryController.java
+++ src/main/java/com/lcwd/electronic/store/controllers/CategoryController.java
@@ -27,7 +27,7 @@ public class CategoryController {
     //create
     @PostMapping
     public ResponseEntity<CategoryDto> createCategory(@Valid @RequestBody CategoryDto categoryDto) {
-        //call service to   save object data into the develop branch cccc
+        //call service to   save object data into the develop branch cccc dndjnlje
         CategoryDto categoryDto1 = categoryService.create(categoryDto);
         return new ResponseEntity<>(categoryDto1, HttpStatus.CREATED);
     }
